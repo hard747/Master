7f1926f9b33bfaf48231959442f92dd841aeb819
harry <harre@ime.usp.br>
2023-10-31 16:42:25 -0500

    rama_prueba

---
+++ comprobando_git/git_branch.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib as mtp
+
+def calcular():
+    print("que desea calcular: ")
+    numero=int(input("ingrese numero"))
+    resultado=numero*numero+2*numero
+    print("este es el resultado " + resultado)
+
+calcular()
+
